2bd0cb2ead96c4653ca8bbfc0912f4e54ebb3a0d
Jahirul83 <jahirulislamcse83@gmail.com>
2022-12-21 20:09:57 +0600

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import routes from "./Routes/Routes/Routes";
 
 function App() {
   return (
-    <div className="w-[1440px] mx-auto">
+    <div className="max-w-[1440px] mx-auto">
       <RouterProvider router={routes}></RouterProvider>
     </div>
   );
