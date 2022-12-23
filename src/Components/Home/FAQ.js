cff8cd623d0efd368e2c6a03119a1938b16d9292
Jahirul83 <jahirulislamcse83@gmail.com>
2022-12-23 19:09:51 +0600

    info & FAQ

--- src/Components/Home/FAQ.js
+++ src/Components/Home/FAQ.js
@@ -8,22 +8,39 @@ const FAQ = () => {
                 <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
                 <div className="flex flex-col divide-y sm:px-8 lg:px-12 xl:px-32 divide-gray-300">
                     <details>
-                        <summary className="py-2 outline-none cursor-pointer focus:underline">Optio maiores eligendi molestiae totam dolores similique?</summary>
+                        <summary className="py-2 outline-none cursor-pointer focus:underline">How your furniture items are technically distinct from that of others?</summary>
                         <div className="px-4 pb-4">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde neque in fugiat magni, quas animi enim veritatis deleniti ex. Impedit.</p>
+                            <p>Apart from using hand-picked raw materials, our furniture items are absolutely nail-free and made of basing on time-honored manufacturing techniques like tongue and groove, finger joints, mortise and tenon, dovetail joints.</p>
                         </div>
                     </details>
                     <details>
-                        <summary className="py-2 outline-none cursor-pointer focus:underline">Modi dolorem veritatis culpa quos consequuntur beatae itaque excepturi perspiciatis?</summary>
+                        <summary className="py-2 outline-none cursor-pointer focus:underline">What’s your Return Policy?</summary>
                         <div className="px-4 pb-4">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est aspernatur quae, eos explicabo odit minima libero veniam similique quibusdam doloribus facilis ipsa accusantium vel maiores corrupti! Libero voluptate a doloribus?</p>
+                            <p>Furniture items in good condition can be returned in 3 days of purchase. This policy is in place to show respect to the notion that a customer may change his/her mind within a certain period of time and that shouldn’t cost him/her a fortune. Only a 10% service charge will be applied on the total amount of purchase in this case. It is not applicable for Fabric product or upholstered product.</p>
                         </div>
                     </details>
                     <details>
-                        <summary className="py-2 outline-none cursor-pointer focus:underline">Magni reprehenderit possimus debitis?</summary>
+                        <summary className="py-2 outline-none cursor-pointer focus:underline">Do you offer any warranty Policy?</summary>
                         <div className="px-4 pb-4 space-y-2">
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut voluptates aspernatur dolores in consequatur doloremque inventore reprehenderit, consequuntur perspiciatis architecto.</p>
-                            <p>Sed consectetur quod tenetur! Voluptatibus culpa incidunt veritatis velit quasi cupiditate unde eaque! Iure, voluptatibus autem eaque unde possimus quae.</p>
+                            <p>We offer 1 year free of cost after sales service warranty which covers any damages caused due to production defects.</p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary className="py-2 outline-none cursor-pointer focus:underline">Do you offer any Exchange Policy?</summary>
+                        <div className="px-4 pb-4 space-y-2">
+                            <p>Furniture can be exchanged in good condition within 3 days with 5% service charge. It is not applicable for customized, set broken and fabric products.</p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary className="py-2 outline-none cursor-pointer focus:underline">Do you offer any Delay Penalty?</summary>
+                        <div className="px-4 pb-4 space-y-2">
+                            <p>Star furniture will pay a delay penalty valuing 0.5% of the value of the goods per week if the delivery is not done within the standard deadline. However, there will be 15 days buffer before any such penalty is applied.</p>
+                        </div>
+                    </details>
+                    <details>
+                        <summary className="py-2 outline-none cursor-pointer focus:underline">Do you offer any Fitting Policy for Knockdown Furniture?</summary>
+                        <div className="px-4 pb-4 space-y-2">
+                            <p>Star furniture will pay a delay penalty valuing 0.5% of the value of the goods per week if the delivery is not done within the standard deadline. However, there will be 15 days buffer before any such penalty is applied.</p>
                         </div>
                     </details>
                 </div>
--- src/Components/Home/Info.js
+++ src/Components/Home/Info.js
@@ -5,13 +5,13 @@ const Info = () => {
         <section className="text-gray-800 mt-20 mb-44">
             <div className="container max-w-xl p-6 py-12 mx-auto space-y-24 lg:px-8 lg:max-w-7xl">
                 <div>
-                    <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl text-primary">Aliquip definiebas ad est</h2>
-                    <p className="max-w-3xl mx-auto mt-4 text-xl text-center text-gray-600">Quando cetero his ne, eum admodum sapientem ut.</p>
+                    <h2 className="text-3xl font-bold tracking-tight text-center sm:text-5xl text-primary">INFORMATION</h2>
+                    <p className="max-w-3xl mx-auto mt-4 text-xl text-center text-gray-600">Best Affordable furniture Price in Bangladesh.</p>
                 </div>
                 <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
                     <div>
-                        <h3 className="text-2xl font-bold tracking-tight sm:text-3xl text-gray-900">Ad vix debet docendi</h3>
-                        <p className="mt-3 text-lg text-gray-600">Ne dicta praesent ocurreret has, diam theophrastus at pro. Eos etiam regione ut, persius eripuit quo id. Sit te euismod tacimates.</p>
+                        <h3 className="text-2xl font-bold tracking-tight sm:text-3xl text-gray-900">Sofa</h3>
+                        <p className="mt-3 text-lg text-gray-600">Choose A Sofa That Makes Your Living Room Look Classy</p>
                         <div className="mt-12 space-y-12">
                             <div className="flex">
                                 <div className="flex-shrink-0">
@@ -22,8 +22,8 @@ const Info = () => {
                                     </div>
                                 </div>
                                 <div className="ml-4">
-                                    <h4 className="text-lg font-medium leading-6 text-gray-900">Per ei quaeque sensibus</h4>
-                                    <p className="mt-2 text-gray-600">Ex usu illum iudico molestie. Pro ne agam facete mediocritatem, ridens labore facete mea ei. Pro id apeirian dignissim.</p>
+                                    <h4 className="text-lg font-medium leading-6 text-gray-900">Looking for a smart, stylish and beautiful sofa to decorate your living room and waiting room?</h4>
+                                    <p className="mt-2 text-gray-600">The living room is seen as the most noisy room in the house. We generally use the living room more or less for entertaining guests, family gatherings or domestic chats. If it comes to the most essential piece of furniture in the living room, then undoubtedly everyone will want to say the name of the sofa.</p>
                                 </div>
                             </div>
                             <div className="flex">
@@ -35,8 +35,8 @@ const Info = () => {
                                     </div>
                                 </div>
                                 <div className="ml-4">
-                                    <h4 className="text-lg font-medium leading-6 text-gray-900">Cu imperdiet posidonium sed</h4>
-                                    <p className="mt-2 text-gray-600">Amet utinam aliquando ut mea, malis admodum ocurreret nec et, elit tibique cu nec. Nec ex maluisset inciderint, ex quis.</p>
+                                    <h4 className="text-lg font-medium leading-6 text-gray-900">Do you want to make your living room more welcoming and attractive to your guests?</h4>
+                                    <p className="mt-2 text-gray-600">So everyone wants to choose a sofa to decorate the living room which will be comfortable to use and will also increase the beauty of the living room manifold.</p>
                                 </div>
                             </div>
                             <div className="flex">
@@ -48,21 +48,21 @@ const Info = () => {
                                     </div>
                                 </div>
                                 <div className="ml-4">
-                                    <h4 className="text-lg font-medium leading-6 text-gray-900">Nulla omittam sadipscing mel ne</h4>
-                                    <p className="mt-2 text-gray-600">At sed possim oporteat probatus, justo graece ne nec, minim commodo legimus ut vix. Ut eos iudico quando soleat, nam modus.</p>
+                                    <h4 className="text-lg font-medium leading-6 text-gray-900">You have come to the right place!</h4>
+                                    <p className="mt-2 text-gray-600">Regal, the most loved and top selling furniture brand in Bangladesh, is ready to serve you with its wide collection of modern, trendy and stylish sofa. Regal’s exquisite collection will not only meet your functional need but also fulfill your aesthetic need. </p>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div aria-hidden="true" className="mt-10 lg:mt-0">
-                        <img src="https://source.unsplash.com/random/360x480" alt="" className="mx-auto rounded-lg shadow-lg bg-gray-500" />
+                        <img src="https://hatil.com/sites/default/files/Moseley-281%20Sofa%20HATIL%20modern%20living%20room%20online%20buy%20Bangladesh%20best%20price.jpg" alt="" className="mx-auto rounded-lg shadow-lg bg-gray-500" />
                     </div>
                 </div>
                 <div>
                     <div className="grid lg:gap-8 lg:grid-cols-2 lg:items-center">
                         <div className="lg:col-start-2">
-                            <h3 className="text-2xl font-bold tracking-tight sm:text-3xl text-gray-900">Eam nibh gloriatur ex</h3>
-                            <p className="mt-3 text-lg text-gray-600">Per odio fabellas consulatu cu. Utroque detracto mel ea, quo te latine theophrastus. Ea his tale nibh dissentias, mei exerci tamquam euripidis cu.</p>
+                            <h3 className="text-2xl font-bold tracking-tight sm:text-3xl text-gray-900">Bed & Bedroom items</h3>
+                            <p className="mt-3 text-lg text-gray-600">We have Luxury beds for more than Just A sound Sleep.</p>
                             <div className="mt-12 space-y-12">
                                 <div className="flex">
                                     <div className="flex-shrink-0">
@@ -73,8 +73,8 @@ const Info = () => {
                                         </div>
                                     </div>
                                     <div className="ml-4">
-                                        <h4 className="text-lg font-medium leading-6 text-gray-900">Cibo augue offendit has ad</h4>
-                                        <p className="mt-2 text-gray-600">An per velit appellantur, ut utinam minimum nominavi sit, odio nostro habemus ne nec. Ne sonet regione contentiones est.</p>
+                                        <h4 className="text-lg font-medium leading-6 text-gray-900">Multi size double bed</h4>
+                                        <p className="mt-2 text-gray-600">A bed usually consumes a lot of space and the size of every bedroom is not the same. So, when you need a double bed, you may want to choose from some options. Don’t worry. This bed is available in almost all regular sizes, such as king size and queen size. You can have your bed according to your need to enjoy a sound sleep.</p>
                                     </div>
                                 </div>
                                 <div className="flex">
@@ -86,8 +86,8 @@ const Info = () => {
                                         </div>
                                     </div>
                                     <div className="ml-4">
-                                        <h4 className="text-lg font-medium leading-6 text-gray-900">At eum ferri luptatum lobortis</h4>
-                                        <p className="mt-2 text-gray-600">Te per quidam maiorum ocurreret, etiam delicatissimi usu ad. Ne has quod periculis. Te sit primis iisque efficiantur.</p>
+                                        <h4 className="text-lg font-medium leading-6 text-gray-900">Great touch of finishing</h4>
+                                        <p className="mt-2 text-gray-600">Star furniture uses lacquer finishing and antique color to add fineness to their furniture. Therefore, it looks shiny and smooth.</p>
                                     </div>
                                 </div>
                                 <div className="flex">
@@ -99,14 +99,14 @@ const Info = () => {
                                         </div>
                                     </div>
                                     <div className="ml-4">
-                                        <h4 className="text-lg font-medium leading-6 text-gray-900">Dicunt verterem evertitur eu sea</h4>
-                                        <p className="mt-2 text-gray-600">Audire principes rationibus eam an, autem nominavi luptatum per te. Sumo fabulas vim eu, sonet saperet eleifend ut vix.</p>
+                                        <h4 className="text-lg font-medium leading-6 text-gray-900">Affordable Price</h4>
+                                        <p className="mt-2 text-gray-600">Our bed prices are quite fair and competitive. Beds are available at prices you can afford. If you consider the build quality, finishing and comfortable nature, the purchase will not cost much.</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="mt-10 lg:mt-0 lg:col-start-1 lg:row-start-1">
-                            <img src="https://source.unsplash.com/random/361x481" alt="" className="mx-auto rounded-lg shadow-lg bg-gray-500" />
+                            <img src="https://hatil.com/sites/default/files/Desire-154_0.jpg" alt="" className="mx-auto rounded-lg shadow-lg bg-gray-500" />
                         </div>
                     </div>
                 </div>
