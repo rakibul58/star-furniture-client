de8b176018a674219a70c7ba5c104bac9e30e13f
rakibul58 <rhrahi14@gmail.com>
2022-12-23 16:40:14 +0600

    rakib done

--- src/Components/Card/Cards.js
+++ src/Components/Card/Cards.js
@@ -1,22 +1,39 @@
 import React, { useContext } from 'react';
+import { toast } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
 import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 import './Cards.css';
 
-function Cards({ service }) {
+function Cards({ services, service, setService}) {
   const { user } = useContext(AuthContext);
+
+  const handleDelete = id => {
+    console.log(id);
+    fetch(`http://localhost:5000/services/${id}`, {
+      method: 'DELETE',
+    })
+      .then(res => res.json())
+      .then(data => {
+        const newServices = services.filter(ser => ser._id !== id);
+        setService(newServices);
+        toast.success('Post Deleted Successfully');
+      });
+  }
+
+
   return (
     <div>
-      <div className=" place-self-auto card bg-base-300 shadow-2xl">
+      <div className="card bg-base-300 shadow-2xl">
         <div className="card-body">
           <figure><img className='Card-design rounded-lg' src={service.image} alt="Shoes" /></figure>
           <h2 className="card-title text-3xl text-primary mt-3">{service.title}</h2>
           <h4 className="card-title">Price: <span className='font-bold text-yellow-500'>${service.price}</span></h4>
-          <p>{service.description}</p>
+          <p>{service.description.length>50 ? service.description.slice(0,50)+'...' : service.description}</p>
           {
             user &&
-            <div className="card-actions justify-end mt-5">
-              <button className="btn btn-xs btn-error text-white hover:bg-opacity-70">Delete</button>
-              <button className="btn btn-xs btn-info text-white hover:bg-opacity-70">Update</button>
+            <div className="card-actions justify-end">
+              <button onClick={() => handleDelete(service._id)} className="btn btn-xs btn-error text-white hover:bg-opacity-70">Delete</button>
+              <Link to={`/updatemodal/${service._id}`} className="btn btn-xs btn-info text-white hover:bg-opacity-70">Update</Link>
             </div>
           }
         </div>
--- src/Components/services/services.js
+++ src/Components/services/services.js
@@ -3,6 +3,7 @@ import Cards from '../Card/Cards';
 
 const Services = () => {
   const [services, setService] = useState([]);
+  const [modalService , setModalService] = useState([]);
 
   useEffect(() => {
     fetch('http://localhost:5000/services')
@@ -23,6 +24,10 @@ const Services = () => {
           services.map(service => <Cards
             key={service._id}
             service={service}
+            setService={setService}
+            services={services}
+            modalService={modalService}
+            setModalService={setModalService}
           ></Cards>)
         }
       </div>
