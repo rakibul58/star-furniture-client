1b5f5664c8f316fdc88691b87a97528ee82e014b
rakibul58 <rhrahi14@gmail.com>
2022-12-23 10:34:42 +0600

    cards and service changed

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,31 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
 	return (
 		<footer className="footer p-10 bg-base-200 text-base-content">
 			<div>
 				<span className="footer-title">Services</span>
-				<a className="link link-hover">Branding</a>
-				<a className="link link-hover">Design</a>
-				<a className="link link-hover">Marketing</a>
-				<a className="link link-hover">Advertisement</a>
+				<Link className="link link-hover">Branding</Link>
+				<Link className="link link-hover">Design</Link>
+				<Link className="link link-hover">Marketing</Link>
+				<Link className="link link-hover">Advertisement</Link>
 			</div>
 			<div>
 				<span className="footer-title">Company</span>
-				<a className="link link-hover">About us</a>
-				<a className="link link-hover">Contact</a>
-				<a className="link link-hover">Jobs</a>
-				<a className="link link-hover">Press kit</a>
+				<Link className="link link-hover">About us</Link>
+				<Link className="link link-hover">Contact</Link>
+				<Link className="link link-hover">Jobs</Link>
+				<Link className="link link-hover">Press kit</Link>
 			</div>
 			<div>
 				<span className="footer-title">Legal</span>
-				<a className="link link-hover">Terms of use</a>
-				<a className="link link-hover">Privacy policy</a>
-				<a className="link link-hover">Cookie policy</a>
+				<Link className="link link-hover">Terms of use</Link>
+				<Link className="link link-hover">Privacy policy</Link>
+				<Link className="link link-hover">Cookie policy</Link>
 			</div>
 			<div>
 				<span className="footer-title">Newsletter</span>
-				<div className="form-control w-80">
+				<div className="form-control">
 					<label className="label">
 						<span className="label-text">Enter your email address</span>
 					</label>
--- src/Components/services/services.js
+++ src/Components/services/services.js
@@ -11,13 +11,21 @@ const Services = () => {
   }, []);
 
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-20'>
-      {
-        services.map(service => <Cards
-        key={service._id}
-        service={service}
-        ></Cards>)
-      }
+    <div className='px-10 mt-12'>
+      <div className='text-center mb-12'>
+        <h4 className='text-2xl font-extrabold font-serif text-primary'>Furniture</h4>
+        <h1 className='text-black text-5xl mb-6'>These are some of our best furniture</h1>
+        <p className='text-base-content'>Star furniture is one of the first furniture brands in Bangladesh to expand globally. Today, this multinational company is proudly representing Bangladeshi furniture in countries like Canada, Australia, USA, Kuwait, UAE, Saudi Arabia, Russia, Egypt, Thailand, India, Nepal, and Bhutan.
+          Built upon the trust of millions of customers, Star furniture has provided premium quality furniture since 1989. Manufactured with the best quality timber, the modern and stylish designed furniture are a signature of elegance and comfort.</p>
+      </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mb-32'>
+        {
+          services.map(service => <Cards
+            key={service._id}
+            service={service}
+          ></Cards>)
+        }
+      </div>
     </div>
   );
 };
