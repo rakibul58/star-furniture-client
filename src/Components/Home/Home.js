6c6b281b00575808351386364002fd8958053e10
rakibul58 <rhrahi14@gmail.com>
2022-12-26 00:30:22 +0600

    changed

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,29 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Contact from '../Contact/Contact';
 import Services from '../services/services';
 import Banner from './Banner';
 import FAQ from './FAQ';
 import Info from './Info';
 import Comments from '../Comments/Comments';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const Home = () => {
+    useEffect(() => {
+        AOS.init();
+      }, []);
     return (
         <div>
             <Banner />
-            <div>
+            <div data-aos="fade-left">
                 <Services />
             </div>
-            <div style={{margin: "200px 0"}}>
+            <div style={{margin: "200px 0"}} data-aos="fade-right">
                 <Comments/>
             </div>
-            <div>
+            <div data-aos="fade-left">
                 <Info />
             </div>
-            <div>
+            <div data-aos="fade-right">
                 <FAQ />
             </div>
-            <div>
+            <div data-aos="fade-left">
                 <Contact />
             </div>
         </div>
